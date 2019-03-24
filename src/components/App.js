bd78df687b7d0a71dd2be56aab77e4b3fecdd9aa
Roxolana <roxolanapavlyk@gmail.com>
2019-03-24 21:39:42 +0100

    created react app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../styles/App.scss'
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <h1>My react app</h1>
+                <p>test</p>
+            </div>
+        )
+    }
+}
